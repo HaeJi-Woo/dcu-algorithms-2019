82aaa8cd466e15edbcd40c260489960fb32e68fd
혜지 우 <whj1939@cu.ac.kr>
2019-12-07 18:07:02 +0900

    버블정렬

--- sorts/sorts.cpp
+++ sorts/sorts.cpp
@@ -3,10 +3,10 @@
 int main()
 {
 	int a[10] = { 3, 4, 2, 6, 7, 8, 3, 4, 1, 5 };
-	int n = 10;
-	for (int i = 0; i < n-1; i++)
+	int k = 10;
+	for (int i = 0; i < k-1; i++)
 	{
-		for (int j = n-1; j > i; j--)
+		for (int j = k-1; j > i; j--)
 		{
 			if (a[j - 1] > a[j])
 				swap(int, a[j - 1], a[j]);
